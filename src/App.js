9d1bb0c07e1e82979de1958d61dd6fa864d8fb9f
Nithin-250 <nithinselvam333@gmail.com>
2025-07-04 11:29:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -178,9 +178,9 @@ function App() {
         </div>
       </div>
 
-       <h1 className="floating-title text-yellow-400 text-4xl md:text-6xl font-starwars text-center drop-shadow-[2px_2px_0_black] mb-6">
-      Talkify: The Podcast Force Awakens
-      </h1>
+        <h1 className="text-yellow-400 text-6xl md:text-8xl font-starwars text-center drop-shadow-[2px_2px_0_black] mb-6">
+        Talkify: The Podcast Force Awakens
+        </h1>
 
       <p className="text-yellow-400 text-2xl md:text-3xl font-starwars text-center drop-shadow-[2px_2px_0_black] mb-12 max-w-2xl">
         Paste a URL below and generate your Star Wars-inspired podcast!
