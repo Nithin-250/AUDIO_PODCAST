fe2f0190d6911b28d3057b8e890a1de5c10f2d0e
Nithin S <nithinselvam333@gmail.com>
2025-10-19 00:21:36 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        starwars: ['"Bebas Neue"', 'sans-serif'],
+      },
+      colors: {
+        starwarsGreen: '#00ff00',
+      },
+    },
+  },
+  plugins: [],
+};
+
+
+
+
